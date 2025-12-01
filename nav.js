d0571ab90b6dd3a999fe3ba33886effab325d67f
Adolf Áron <adolfaron@turr.hu>
2025-12-01 11:35:13 +0100

    mobil

--- nav.js
+++ nav.js
@@ -1,11 +1,27 @@
 let nyitva = false;
+const nav = document.getElementById("nav");
 function nyitZar(){
-    if (nyitva) {
-        document.getElementById("nav").style.display ="block" 
+    if (!nyitva) {
+        nav.style.display ="block" 
         nyitva = true;
     }
     else {
-        document.getElementById("nav").style.display ="none"
+        nav.style.display ="none"
         nyitva = false;
     }
 }
+window.addEventListener("resize", function() {
+    
+    if (window.innerWidth > 720) {
+        nav.style.display = "";
+    } else {
+        if (nyitva) {
+        nav.style.display ="block" 
+        
+        }
+        else {
+            nav.style.display ="none"
+            
+        }
+    }
+});
