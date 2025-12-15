8f96a98417ed705f54d84556f56ecc737f863897
Adolf Áron <adolfaron@turr.hu>
2025-12-15 11:35:21 +0100

    nav animáció kész

--- nav.js
+++ nav.js
@@ -3,24 +3,30 @@ const nav = document.getElementById("nav");
 
 function nyitZar() {
     if (!nyitva) {
-        nav.style.maxHeight = "150px";
-        nav.style.display ="block"
+        nav.style.maxHeight = "200px";
+        nav.style.display ="flex"
         nyitva = true;
     } else {
         nav.style.maxHeight = "0px";
-        nav.style.display ="block"
+        nav.style.display ="flex"
         nyitva = false;
     }
 }
 
 window.addEventListener("resize", function () {
     if (window.innerWidth < 768) {
-        // desktop: mindig látszik, nincs animáció
-        nav.style.maxHeight = nyitva ? "150px" : "0px";
-        //nav.style.maxHeight = "150px";
+        nav.style.transition = "none"
+        nav.style.maxHeight = nyitva ? "200px" : "0px";
+        setTimeout(() => {
+            nav.style.transition = "max-height .8s ease-in-out"
+          }, 800);
     } else {
         // mobil: az állapottól függ
-        //nav.style.maxHeight = nyitva ? "150px" : "0px";
-        nav.style.maxHeight = "150px";
+        //nav.style.maxHeight = nyitva ? "200px" : "0px";
+        nav.style.transition = "none"
+        nav.style.maxHeight = "200px";
+        setTimeout(() => {
+            nav.style.transition = "max-height .8s ease-in-out"
+          }, 800);
     }
 });
